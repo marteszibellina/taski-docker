79d36564e0d5f89ae2eb90b062eb6bb37e2e3ec0
marteszibellina <valerababahov@yandex.ru>
2025-02-12 15:33:23 +0300

    Updated Settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost',]
+ALLOWED_HOSTS = ['158.160.88.6', 'valerababahov.onthewifi.com', 'localhost',]
 
 
 # Application definition
